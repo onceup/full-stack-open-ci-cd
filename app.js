3e884ec06521e401502ec43a5a439146334d250f
onceup <chistoyo@gmail.com>
2022-11-16 20:30:18 +0100

    [DELETE] delete error throwing

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5100
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw new Error('error...');
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
